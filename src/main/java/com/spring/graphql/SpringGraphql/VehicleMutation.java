02052aa555eec9408b494f53d664f422924008aa
Neeraj722 <neerajsinha002@gmail.com>
2020-01-24 00:47:07 +0530

    GraphQL Project with Spring Boot

---
+++ src/main/java/com/spring/graphql/SpringGraphql/VehicleMutation.java
@@ -0,0 +1,19 @@
+package com.spring.graphql.SpringGraphql;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+    @Autowired
+    private VehicleService vehicleService;
+    public Vehicle createVehicle(final String type, final String modelCode, final String brandName, final String launchDate) {
+        return this.vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+    }
+
+    public boolean deleteVehicle(Integer id) {
+        this.vehicleService.deleteVehicle(id);
+        return true;
+    }
+}
